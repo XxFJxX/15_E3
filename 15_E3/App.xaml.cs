d5a2f13631fa54915bf9edb49c8cd67f1cfc9426
XxFJxX <ferinocente357@gmail.com>
2023-06-30 23:36:59 -0600

    Agregar archivos de proyecto.

---
+++ 15_E3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _15_E3;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ 15_E3/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace _15_E3;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        string N = Nom.Text;
+        double pkg = Convert.ToDouble(kg.Text);
+        double amts = Convert.ToDouble(mts.Text);
+
+        double pt = pkg * 2.20462;
+        double at = amts * 100;
+
+        la.Text = "EL DINOSAURIO " + N + " pesa en kg =" + pkg + " en libras =" + pt +
+            " de alto en metros =" + amts + " en centimetros =" + at;
+    }
+}
+
